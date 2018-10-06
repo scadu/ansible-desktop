8b02388067afed5ba7cc3e5715d9e0ff1d617aa5
Łukasz Jendrysik <scadu@disroot.org>
2018-10-06 21:42:22 +0200

    Add vscode role as submodule, add gitignore

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if ! [ -x "$(command -v ansible)" ]; then
+  echo 'Ansible not found. Installing...' >&2
+  sudo dnf install -y ansible
+fi
+
+ansible-playbook deploy.yml -vv -D
