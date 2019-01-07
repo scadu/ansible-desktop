5be8e29e30ad21329e59c38c2949961332e36546
Łukasz Jendrysik <scadu@disroot.org>
2019-01-07 20:25:40 +0100

    Install ansible in venv

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-if ! [ -x "$(command -v ansible)" ]; then
-  echo 'Ansible not found. Installing...' >&2
-  pip install --user 'ansible==2.6.8'
+export PATH="$HOME/.local/bin:$PATH"
+
+if ! [ -x "$(command -v pip2)" ]; then
+  echo 'pip2 not found. Installing...' >&2
+  sudo apt install python-pip -y
 fi
+
+echo 'Installing Ansible...' >&2
+pip2 install virtualenv && pip install --user virtualenv
+virtualenv venv && source venv/bin/activate
+pip2 install -r requirements.txt
 
 ansible-playbook deploy.yml -vv -D -K
