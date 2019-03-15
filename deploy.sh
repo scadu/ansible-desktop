4e4ed4a30a382424edf78ef11c500286078d0601
Łukasz Jendrysik <scadu@disroot.org>
2019-03-15 17:32:36 +0100

    Deploy script: install ansible if not available on the system

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt update
-sudo apt install ansible -y
+if ! command -v ansible >/dev/null 2>&1
+then
+  sudo apt-add-repository ppa:ansible/ansible -y
+  sudo apt update
+  sudo apt install ansible -y
+fi
 
 ansible-playbook deploy.yml -vv -D -K
