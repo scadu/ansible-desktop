19b270b9bc43c862f0e9f894a92b73e1065cbce9
Łukasz Jendrysik <scadu9@gmail.com>
2019-09-28 15:28:57 +0200

    Minor improvements to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
+
+sudo apt update && sudo apt install software-properties-common -y
 
 if ! command -v ansible >/dev/null 2>&1
 then
-  sudo apt-add-repository ppa:ansible/ansible -y
-  sudo apt update
+  sudo apt-add-repository -y -u ppa:ansible/ansible 
   sudo apt install ansible -y
 fi
 
